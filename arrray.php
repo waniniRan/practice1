3406f1ad58d1bae1cdde778cf8430d9d012858e9
waniniRan <cindy.gichuhi@strathmored.edu>
2024-05-24 10:42:10 +0300

    new php name

---
+++ arrray.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+
+$color=["Black","Green","Yellow"];
+
+print_r($color);
+
+?>
+
+<br>
+<?php
+$user= array("Alex","Peter","Ann";)
+?>
+
+<pre>
+    <?php print_r($user); ?>
+    </pre>
